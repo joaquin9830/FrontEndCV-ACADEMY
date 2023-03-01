d6deb0cea637302e774b9255bf7c4871858b5ce1
joaquin9830 <joaquin9811@gmail.com>
2023-03-01 12:01:58 -0300

    Primer commit

---
+++ scripts/funciones.js
@@ -0,0 +1,7 @@
+
+document.getElementById('criminalista').onclick = function(){
+   
+    document.getElementById('textocrimi').style.display='block';
+    document.getElementById('textocrimi2').style.display='block';
+}
+
